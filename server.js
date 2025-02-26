416922e3490c77f7ff02ec1c7583d8c6a18e35b2
Mj <markjoseph.aspe@gmail.com>
2025-02-26 18:57:46 +0800

    Features: Catching Uncaught Exceptions & Handle unhandled promise rejections

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import app from './app.js';
 import connectDB from './config/db.js';
 
 
+// Catching Uncaught Exceptions
+process.on('uncaughtException', err => {
+    console.error(`Uncaught Exception: ${err.message}`);
+    process.exit(1);
+});
+
 // Load environment variables
 dotenv.config({ path: './config.env' });
 
@@ -12,6 +18,12 @@ connectDB();
 
 // Start Express Server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (reason, promise) => {
+    console.error(`Unhandled Rejection at: ${promise}. Reason: ${reason}`);
+    server.close(() => process.exit(1));
 });
