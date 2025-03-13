d889dd44b95f3567cc404443158330e5da238dd2
Mj <markjoseph.aspe@gmail.com>
2025-03-13 14:46:24 +0800

    Features: Managing Password

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import slugify from "slugify";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -25,10 +26,30 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password!'],
-        minLength: 8
+        validate: {
+            // Only Works on CREATE or SAVE !!!
+            validator: function(el){
+                return el === this.password;
+            },
+            message: 'Password does not match!'
+        }
     }
+});
+
+userSchema.pre('save', async function(next){
+    // Only run this function if password was actually modified
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    //  Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+export default User; 
