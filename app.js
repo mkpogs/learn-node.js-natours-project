0dfcdb4f71975c8247561b324b0534286db59fc1
Mj <markjoseph.aspe@gmail.com>
2025-03-20 15:48:07 +0800

    Features:Preventing Parameter Pollution - Fix Whitelisting

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ import AppError from './utils/appError.js';
 import { globalErrorHandler } from './controllers/errorController.js'
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
-import { whitelist } from 'validator';
 
 dotenv.config({
     path: './config.env'
@@ -53,24 +52,24 @@ app.use(mongoSanitize());
 app.use(xss());
 
 // Prevent parameter pollution
-app.use(hpp());
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'maxGroupSize',
+        'difficulty',
+        'ratingsAverage',
+        'ratingsQuantity',
+        'price',
+        'sort'
+    ]
+}));
 
 app.use(express.static(`${__dirname}/src`)); // Serving a static file
 
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    next({
-        whitelist: [
-            'duration',
-            'maxGroupSize',
-            'difficulty',
-            'ratingsAverage',
-            'ratingsQuantity',
-            'price',
-            'sort'
-        ]
-    });
+    next();
 });
 
 // Mounting Routes
