5092f94c0f51a38a26d575dbfd4a6e59515cf318
Mj <markjoseph.aspe@gmail.com>
2025-03-21 14:28:11 +0800

    Modelling Tour_Guides_Child Referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import slugify from "slugify";
 import validator from "validator";
-import User from "./userModel.js";
+// import User from "./userModel.js";
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -102,7 +102,12 @@ const tourSchema = new mongoose.Schema({
             day: Number
         }
     ],
-    guides: Array
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -119,12 +124,12 @@ tourSchema.pre('save', function(next){
     next();
 });
 
-tourSchema.pre('save', async function(next){
-    const guidesPromises = this.guides.map(async id => await User.findById(id));
-    this.guides = await Promise.all(guidesPromises);
+// tourSchema.pre('save', async function(next){
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
 
-    next();
-});
+//     next();
+// });
 
 // tourSchema.post('save', function(doc, next){
 //     console.log(doc);
