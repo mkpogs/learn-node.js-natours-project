0f5b143eb0a91984d9108d29f057d400983142ae
Mj <markjoseph.aspe@gmail.com>
2025-04-03 14:45:46 +0800

    Building TOur Overview - Tour Card Template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,21 @@
+import Tour from './../models/tourModel.js';
+import catchAsync from './../utils/catchAsync.js';
+import AppError from './../utils/appError.js';
 
-export const getOverview = (req, res) => {
+export const getOverview = catchAsync(async(req, res, next) => {
+    // 1. Get tour data from collection
+    const tours = await Tour.find();
+    // 2. BUild Template
+
+    // 3. Render that template using data from 1
+
+
+
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-}
+});
 
 export const getTour = (req, res) => {
     res.status(200).render('tour', {
